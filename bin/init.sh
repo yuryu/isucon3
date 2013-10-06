b3679da035ebba0a91e67e60fccf2c79a55c9242
matsuken <matsukena@gmail.com>
2013-10-06 16:13:14 +0900

    トランザクションの処理が失敗してたので

--- bin/init.sh
+++ bin/init.sh
@@ -3,5 +3,5 @@
 `/usr/bin/mysql -uroot -proot isucon -e"ALTER TABLE memos ADD username varchar(255) NOT NULL DEFAULT '' AFTER user"`;
 `/usr/bin/mysql -uroot -proot isucon -e"ALTER TABLE memos ADD INDEX user(user)"`;
 `/usr/bin/mysql -uroot -proot isucon -e"ALTER TABLE memos ADD INDEX is_private(is_private)"`;
-`/usr/bin/mysql -uroot -proot isucon -e"BEGIN TRANSACTION; UPDATE memos INNER JOIN users on memos.user = users.id SET memos.username = users.username"; COMMIT`;
+`/usr/bin/mysql -uroot -proot isucon -e"UPDATE memos INNER JOIN users on memos.user = users.id SET memos.username = users.username"`;
 
