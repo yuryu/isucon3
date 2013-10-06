33c1df9251be68590c80a637f21a8bc644e322bc
s_wool <s_wool@mac.com>
2013-10-06 14:45:16 +0900

    modified init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -2,5 +2,5 @@
 
 `/usr/bin/mysql -uroot -proot isucon -e"ALTER TABLE memos ADD username varchar(255) NOT NULL DEFAULT '' AFTER user"`;
 
-`/usr/bin/mysql -uroot -proot isucon -e"UPDATE memos INNER JOIN users on memos.user = users.id SET memos.username = users.username"`;
+`/usr/bin/mysql -uroot -proot isucon -e"BEGIN TRANSACTION; UPDATE memos INNER JOIN users on memos.user = users.id SET memos.username = users.username"; COMMIT`;
 
